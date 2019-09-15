1f6d0f662515d2a1cd7eac5ad4e1cb6ddb4389b8
LiquidFlare <46068905+LiquidFlare@users.noreply.github.com>
2019-09-15 16:23:43 +0530

    loads moderation cog now

--- Bot.py
+++ Bot.py
@@ -8,5 +8,6 @@ bot = commands.Bot(command_prefix=">")
   # TODO: add variable prefix
 
 bot.load_extension("cogs.General")
+bot.load_extension("cogs.Moderation")
 
 bot.run("NTg4MDI3OTU0NjU1Nzg5MTEx.XP_KUA.hfVm5j4a-g5H0U7ugehGs1i1yhQ")
