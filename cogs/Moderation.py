b12f6d34a5e5f6f4027bba1f450dd6a6cb1b69af
LiquidFlare <46068905+LiquidFlare@users.noreply.github.com>
2019-09-15 16:23:00 +0530

    moderation cog

---
+++ cogs/Moderation.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+
+class Moderation(commands.Cog, name = "Mod"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def maodmanual(self, ctx):
+        embed = discord.Embed(title="Help on Byte", description="Staff manual", color=0xdc322f,
+                          timestamp=datetime.utcfromtimestamp(1559025608))
+        embed.add_field(name=">kick", value="Kicks the mentioned user")
+        embed.add_field(name=">ban", value="Bans the mentioned user")
+        embed.add_field(name=">purge", value="Deletes specified number of messages")
+        embed.set_footer(text="Byte Alpha", icon_url="https://cdn.discordapp.com/attachments/497783794091294740/"
+                                                 "582817787974516763/abstract-abstract-painting-art-2230796.jpg")
+        await ctx.send(content=None, embed=embed)
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member : discord.Member, *, reason: str = None):
+        if member == None:
+            await ctx.send("Please specify a user to be kicked")
+        if reason == None:
+            await discord.Member.kick(member, reason=reason)
+            await ctx.send("Kicked " +  str(member) + " for no reason xD")
+        else:
+            await discord.Member.kick(member, reason=reason)
+            await ctx.send("Kicked " + str(member) + " for " + reason)
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
