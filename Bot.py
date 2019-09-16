4c7360e4a1f948982cca86a9d9f55ca19b6e4ef4
LiquidFlare <46068905+LiquidFlare@users.noreply.github.com>
2019-09-16 19:03:51 +0530

    added universal reload command

--- Bot.py
+++ Bot.py
@@ -7,7 +7,24 @@ Client = discord.Client()
 bot = commands.Bot(command_prefix=">")
   # TODO: add variable prefix
 
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def reload(ctx):
+  await ctx.send("Reloading bot.")
+  await ctx.send("Unloading General")
+  bot.unload_extension("cogs.General")
+  await ctx.send("Unloading Moderation")
+  bot.unload_extension("cogs.Moderation")
+  await ctx.send("Unloading Fun")
+  bot.unload_extension("cogs.Fun")
+  await ctx.send("Extensions fully unloaded. Reloading.")
+  bot.load_extension("cogs.General")
+  bot.load_extension("cogs.Moderation")
+  bot.load_extension("cogs.Fun")
+  await ctx.send("Reloaded. Status: fully functional.")
+
 bot.load_extension("cogs.General")
 bot.load_extension("cogs.Moderation")
+bot.load_extension("cogs.Fun")
 
-bot.run("NTg4MDI3OTU0NjU1Nzg5MTEx.XP_KUA.hfVm5j4a-g5H0U7ugehGs1i1yhQ")
+bot.run(TOKEN)
